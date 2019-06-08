693df3c5fb67a3a3ada1bdbae0caba4336fee5c3
jatin kukreja <jatinkukreja18@yahoo.in>
2019-06-08 17:51:09 +0530

    changes in step 1

--- src/js/app.js
+++ src/js/app.js
@@ -2,3 +2,8 @@ import "../stylesheets/main.scss";
 // require('file-loader?name=[name].[ext]!../../index.html');
 
 import {modal}  from "./custom/modal";
+
+ function enableButton(selector){    
+    document.querySelector(selector).disabled = false;
+}
+// console.log(document.querySelector('body'))
